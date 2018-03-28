faa3732741658b5c4276c09daebdc69929437840
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-28 10:29:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { Eminem: 'Lose Yourself' }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    return Object.assign(playlist, { [artist]: songTitle })
+    return Object.assign(playlist, { [artistName]: songTitle })
 }
 
 function removeFromPlaylist(myPlaylist, artistName) {
